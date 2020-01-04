d8d3603ab578a4e55d4bf465c250989419e0df3f
samtaaghol <35299367+samtaaghol@users.noreply.github.com>
2020-01-04 12:40:44 +0000

    Update r6.py

--- r6/r6.py
+++ r6/r6.py
@@ -8,25 +8,60 @@ cv2.resizeWindow('image', 800,600)
 
 import os
 
-# Contains the frontalface haarcascade.
-face_cascade = cv2.CascadeClassifier('C:\\Users\\staag\\Downloads\\haarcascade_upperbody.xml')
-
-color = (100,100,255) # BGR
-stroke = 2
+# Load Yolo
+net = cv2.dnn.readNet("C:\\Users\\staag\\OneDrive\\Documents\\r6\\yolov3.weights", "C:\\Users\\staag\\OneDrive\\Documents\\r6\\yolov3.cfg")
+classes = []
+with open("C:\\Users\\staag\\OneDrive\\Documents\\r6\\coco.names", "r") as f:
+    classes = [line.strip() for line in f.readlines()]
+layer_names = net.getLayerNames()
+output_layers = [layer_names[i[0] - 1] for i in net.getUnconnectedOutLayers()]
+colors = np.random.uniform(0, 255, size=(len(classes), 3))
 
 while True:
-    img = pyscreenshot.grab(bbox = (5,60,1024,768 + 32))
+    img = pyscreenshot.grab(bbox = (30,60,1024,768 + 32))
     img_np = np.array(img)
-
-    grayscale = cv2.cvtColor(img_np,cv2.COLOR_BGR2GRAY)
-    faces = face_cascade.detectMultiScale(grayscale,1.1, 10)
-
-    for (a,b,d,e) in faces:
-        print("found")
-        roi_gray = grayscale[b:b+e,a:a+d]
-        end_cord_a = a + d
-        end_cord_b = b + e
-        cv2.rectangle(img_np, (a,b), (end_cord_a,end_cord_b),color,stroke)
+    height, width, channels = img_np.shape
+    img_np = cv2.UMat(img_np)
+    
+    
+    # Detecting objects
+    blob = cv2.dnn.blobFromImage(img_np, 0.00392, (416, 416), (0, 0, 0), True, crop=False)
+    net.setInput(blob)
+    outs = net.forward(output_layers)
+    # Showing informations on the screen
+    class_ids = []
+    confidences = []
+    boxes = []
+    for out in outs:
+        for detection in out:
+            scores = detection[5:]
+            class_id = np.argmax(scores)
+            confidence = scores[class_id]
+            if confidence > 0.5:
+                print('detected')
+                
+                # Object detected
+                center_x = int(detection[0] * width)
+                center_y = int(detection[1] * height)
+                w = int(detection[2] * width)
+                h = int(detection[3] * height)
+                
+                # Rectangle coordinates
+                x = int(center_x - w / 2)
+                y = int(center_y - h / 2)
+                boxes.append([x, y, w, h])
+                confidences.append(float(confidence))
+                class_ids.append(class_id)
+                           
+    indexes = cv2.dnn.NMSBoxes(boxes, confidences, 0.5, 0.4)
+    font = cv2.FONT_HERSHEY_PLAIN
+    for i in range(len(boxes)):
+        if i in indexes:
+            x, y, w, h = boxes[i]
+            label = str(classes[class_ids[i]])
+            color = colors[i]
+            cv2.rectangle(img_np, (x, y), (x + w, y + h), color, 2)
+            cv2.putText(img_np, label, (x, y + 30), font, 3, color, 3)
 
     cv2.imshow("image", img_np)
     if cv2.waitKey(1) == 27:
